f01abb510c9a1b88937272f0fc64899deae804aa
Jacob Schwartz <jaschwa@umich.edu>
2022-01-14 12:27:35 -0500

    clean imports

--- jhs_test.py
+++ jhs_test.py
@@ -1,16 +1,11 @@
-import io
-import msgpack
-import msgpack_numpy as m
 from triplets_format import TripletsFormat
 import numpy as np
-
-m.patch()
+import torch
 
 arr1 = np.random.rand(441, 1, 1).astype('float16')
 arr2 = np.random.rand(441, 1000, 1000).astype('float16')
 arr3 = np.random.rand(441, 5, 5).astype('float16')
 arr4 = np.random.rand(441, 50, 50).astype('float16')
-
 
 with TripletsFormat(file_name='temp.bigdata', map_name='map.bigmap', mode='w') as j:
 	j.write_next_np(arr1, '#1')
@@ -29,6 +24,8 @@ with TripletsFormat(file_name='temp.bigdata', map_name='map.bigmap', mode='r') a
 
 	result, identifier = j.read_next_np()
 	assert np.all(result == arr2)
+
+	tensor = torch.from_numpy(arr2)
 
 	result, identifier = j.read_next_np()
 	assert np.all(result == arr3)
